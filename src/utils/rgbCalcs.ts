bec72e6c9703dc837def108d2e4c503c7ef292be
Shaun Farrell <>
2021-01-31 19:13:39 +0000

    add type

--- src/utils/rgbCalcs.ts
+++ src/utils/rgbCalcs.ts
@@ -63,7 +63,7 @@ export const calcScale = (
   return inverse ? 255 - rounded : rounded;
 };
 
-const componentToHex = (c) => {
+const componentToHex = (c: number) => {
   var hex = c.toString(16);
   return hex.length === 1 ? "0" + hex : hex;
 };
